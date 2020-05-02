143758db578eed23a5943ca1a801fc140423ea5f
danlibs <danlibs00@gmail.com>
2020-05-02 16:39:10 -0300

    Added basic movements
    Player can walk and jump, all animated.

---
+++ Bake Time Shot/Assets/Scripts/GroundCheck.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCheck : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            this.GetComponentInParent<Player>().IsGrounded = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            this.GetComponentInParent<Player>().IsGrounded = false;
+        }
+    }
+}
--- Bake Time Shot/Assets/Scripts/Player.cs
+++ Bake Time Shot/Assets/Scripts/Player.cs
@@ -4,15 +4,67 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float Speed;
+    public float JumpForce;
+    public bool IsGrounded;
+
+    private Rigidbody2D rb;
+    private Animator anim;
+    [SerializeField]
+    private bool facingRight;
+    [SerializeField]
+    private bool jumping = false;
+    private float xMov;
+
+    private void Start()
     {
-        
+        rb = this.GetComponent<Rigidbody2D>();
+        anim = this.GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        xMov = Input.GetAxisRaw("Horizontal");
+
+        if (IsGrounded)
+        {
+            if (Input.GetKeyDown(KeyCode.Space) && !jumping)
+            {
+                rb.AddForce(Vector2.up * JumpForce * Time.fixedDeltaTime, ForceMode2D.Impulse);
+            }
+            else if (xMov == 0)
+            {
+                anim.Play("Idle");
+            }
+            else if (xMov != 0)
+            {
+                anim.Play("Walking");
+            }
+        }
+        else
+        {
+            anim.Play("Jumping");
+        }       
+
+        if (xMov < 0 && facingRight)
+        {
+            Flip();
+        }
+        else if (xMov > 0 && !facingRight)
+        {
+            Flip();
+        }
+    }
+
+    private void FixedUpdate() {
+        rb.velocity = new Vector2(xMov * Speed * Time.fixedDeltaTime, rb.velocity.y);
+
+
+    }
+
+    private void Flip()
+    {
+        facingRight = !facingRight;
+        this.transform.Rotate(0, 180, 0);
     }
 }
